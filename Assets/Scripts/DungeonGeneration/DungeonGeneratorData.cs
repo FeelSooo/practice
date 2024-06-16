52a26a1dddaa4ba34e128bd1f2e70719eeb63ba1
Ivan Abramov <abrshark3@gmail.com>
2024-06-16 17:59:14 +0300

    Создал алгоритм генерации подземелья

---
+++ Assets/Scripts/DungeonGeneration/DungeonGeneratorData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DungeonGeneratorData.asset", menuName = "DungeonGeneratorData/Dungeon Data")] 
+
+public class DungeonGeneratorData : ScriptableObject
+{
+    public int numberOfCrawlers;
+    public int iterationMin;
+    public int iterationMax;
+}
